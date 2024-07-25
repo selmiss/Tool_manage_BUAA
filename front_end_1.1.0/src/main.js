28bfd473de07763225f14eb7616a18058a493ff4
implank <tense_chen@outlook.com>
2024-07-25 22:06:53 +0800

    rebuild frontend and add gitignore

--- front_end_1.1.0/src/main.js
+++ front_end_1.1.0/src/main.js
@@ -13,7 +13,7 @@ Vue.use(ElementUI);
 Vue.prototype.axios=axios
 // axios.defaults.baseURL='http://127.0.0.1:8000/'
 // axios.defaults.baseURL='http://tool.selmiss.com/'
-axios.defaults.baseURL='http://121.4.160.157/'
+axios.defaults.baseURL='http://10.212.87.185/'
 
 
 
