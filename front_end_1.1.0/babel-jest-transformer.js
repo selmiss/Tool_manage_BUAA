8d6dc6b813b7f43d2d05d9351fb45fbee2dd58f5
implank <tense_chen@outlook.com>
2024-09-26 18:27:15 +0800

    fk

--- front_end_1.1.0/babel-jest-transformer.js
+++
@@ -1,8 +0,0 @@
-const babelConfig = {
-  // ... whatever else ...
-  plugins: [
-      '@babel/plugin-transform-modules-commonjs',
-  ],
-};
-
-module.exports = require('babel-jest').createTransformer(babelConfig);
--- front_end_1.1.0/babel.config.js
+++ front_end_1.1.0/babel.config.js
@@ -1,20 +1,5 @@
-// module.exports = {
-//   presets: [
-//     '@vue/cli-plugin-babel/preset'
-//   ]
-// }
-
 module.exports = {
-  // ... whatever else ...
-  globals: {
-    'vue-jest': {
-      transform: {
-        js: './babel-jest-transformer.js',
-      },
-    },
-  },
-  transform: {
-    '.*\\.js$': './babel-jest-transformer.js',
-    '.*\\.vue$': '@vue/vue2-jest',
-  },
-};
+  presets: [
+    '@vue/cli-plugin-babel/preset'
+  ]
+}
