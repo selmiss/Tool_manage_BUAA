3db0d900f70b19f7a9cd173daa011bc337a27d20
implank <tense_chen@outlook.com>
2024-09-26 20:16:45 +0800

    fk

--- front_end_1.1.0/vite.config.js
+++ front_end_1.1.0/vite.config.js
@@ -8,9 +8,9 @@ import legacy from '@vitejs/plugin-legacy'
 export default defineConfig({
   plugins: [
     vue(),
-    // legacy({
-    //   targets: ['defaults', 'not IE 11']
-    // })
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    })
   ],
   resolve: {
     extensions: ['.js', '.vue'],
